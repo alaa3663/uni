20e260b5ca606c3619bf54dfe4d0bec2d37358da
Alaa <alaaaad77@gmail.com>
2026-01-25 01:22:13 +0100

    sign out button fixed

--- react-App/src/components/sidebar/SideBar.jsx
+++ react-App/src/components/sidebar/SideBar.jsx
@@ -3,8 +3,16 @@ import { MdDashboard, MdLogout } from "react-icons/md";
 import { FaBook, FaChartBar, FaCalendarTimes, FaUser } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 import "./SideBar.css";
+import { useNavigate } from 'react-router-dom';
 
 function SideBar() {
+    const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // Add logout logic here (clear tokens, etc.)
+    console.log('Logging out...');
+    navigate('/');
+  };
   return (
     <div className="sideBar">
       <p><IoBookSharp />University Companion</p>
@@ -29,9 +37,11 @@ function SideBar() {
           <FaUser /> Profile
         </NavLink>
       </div>
-      <div className="footer">
-        <button><MdLogout /> Sign Out</button>
-      </div>
+    <div className="footer">
+      <button onClick={handleLogout}>
+        <MdLogout /> Sign Out
+      </button>
+    </div>
     </div>
   );
 }
